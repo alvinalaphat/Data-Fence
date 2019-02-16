63d5b76d80ad7114d3fc42acf7ea193c9e6ca1e7
rkramer379 <2021379@sluh.org>
2019-02-16 16:23:19 -0600

    added location and front end

--- pages/index2.js
+++ pages/index2.js
@@ -1,5 +1,5 @@
 var modal = document.getElementById("modalbox");
-
+var answers = [];
 function show() {
   modal.style.display = "block";
 }
@@ -13,16 +13,64 @@ yes.onclick = function() {
   console.log("Yes");
   modal.style.display = "none";
   open();
+  getLocation();
 };
 
 no.onclick = function() {
   modal.style.display = "none";
   console.log("No");
   open();
+  getLocation2();
 };
 
 function open() {
   setTimeout(function() {
     modal.style.display = "block";
-  }, 3000);
+  }, 10000);
+}
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  console.log([position.coords.latitude, position.coords.longitude]);
+  var long = position.coords.longitude;
+  var lat = position.coords.latitude;
+  var answ = "yes";
+  var response = {
+    longitude: long,
+    latitude: lat,
+    answer: "yes"
+  };
+  var myJson = JSON.stringify(response);
+  document.getElementById("demo2").innerHTML = myJson;
+}
+
+function getLocation2() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition2);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition2(position) {
+  console.log([position.coords.latitude, position.coords.longitude]);
+  var long = position.coords.longitude;
+  var lat = position.coords.latitude;
+  var answ = "no";
+  var response = {
+    longitude: long,
+    latitude: lat,
+    answer: "no"
+  };
+  var myJson = JSON.stringify(response);
+  document.getElementById("demo2").innerHTML = myJson;
 }
