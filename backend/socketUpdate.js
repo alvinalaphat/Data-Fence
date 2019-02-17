add4e8a2981a959d1d7d142974698e982006367d
Micah See <exploretheworldofdell@gmail.com>
2019-02-17 00:46:01 -0600

    rearrange

---
+++ backend/socketUpdate.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+const findIntruder = require('')
+var locationData = [];
+var timeout = 30;
+var open = true;
+var socket = 3000;
+
+
+io.on('connection', function(socket) {
+
+  socket.on('locationUpdate', function(locationData) {
+
+    if (locationData.length == 0) {
+      setTimeout(function() {
+        open = false;
+        //put the location prediction and emit the update predictions to the users
+      }, timeout * 1000);
+    }
+
+    if (answer == 'yes' && open) {
+      locationData.push([req.body.latitude, req.body.longitude])
+    }
+
+  });
+});
